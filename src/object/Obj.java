3d30b8370783dab5ec87ec00e25713734581a22a
yianxitong <31850610+yianxitong@users.noreply.github.com>
2020-05-19 19:46:32 +0800

    试试hebing

--- src/object/Obj.java
+++ src/object/Obj.java
@@ -85,6 +85,7 @@ public class Obj extends JPanel
 		case 31:g.drawImage(Toolkit.getDefaultToolkit().getImage("./Í¼Æ¬ËØ²Ä/Ö÷½Ç/¹¥»÷/hero1_atk136.png"),x-115,y-70,this);break;
 		case 32:g.drawImage(Toolkit.getDefaultToolkit().getImage("./Í¼Æ¬ËØ²Ä/Ö÷½Ç/ÅÀÉş×Ó/0.png"),x-27,y-70,this);break;
 		case 33:g.drawImage(Toolkit.getDefaultToolkit().getImage("./Í¼Æ¬ËØ²Ä/Ö÷½Ç/ÅÀÉş×Ó/0.png"),x-27,y-70,this);break;
+		
 		}
 		g.drawString("ÆÁÄ»Î»ÖÃ£º"+"x="+Integer.toString(x)+"y="+Integer.toString(y),645,525);
 	}
